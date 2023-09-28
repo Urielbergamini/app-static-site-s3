5b5919971f9bfd300b946e0c4f0063aa9b95101f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-27 21:50:18 -0300

    vai

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 # PROVIDER
 terraform {
 
-  required_version = "~> 1.2"
+  required_version = "~> 1.4"
 
   required_providers {
     aws = {
@@ -9,4 +9,12 @@ terraform {
       version = "~> 4.55"
     }
   }
+
+  backend "s3" {
+    bucket         = "tf-staticsite-s3-state-v1"
+    key            = "terraform.tfstate"
+    dynamodb_table = "tf-staticsite-s3-state-v1"
+    region         = "us-east-1"
+  }
+
 }
