e0ccf8b75284cbd36a7f0d59f56d36ff80c2eead
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-27 21:55:58 -0300

    jsdiaojsai

--- terraform/modules/s3/output.tf
+++ terraform/modules/s3/output.tf
@@ -1,7 +1,3 @@
-resource "aws_s3_bucket_acl" "bucket-acl" {
-  bucket = aws_s3_bucket.bucket.id
-  acl    = "public-read"
-}
 
 output "aws_s3_bucket_website_endpoint" {
   value = "http://${var.website_endpoint == "true" ? aws_s3_bucket_website_configuration.bucket-website-configuration.website_endpoint : ""}"
